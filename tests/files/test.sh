9e0fb198090973763de184a442519cdd04dffa50
Brett Tasker <brett.tasker@gmail.com>
2020-08-08 19:15:00 +1200

    Add FPM and CLI builds

--- tests/files/test.sh
+++ tests/files/test.sh
@@ -1,23 +1,59 @@
 #!/bin/bash
 set -euo pipefail
 
-try_curl() {
-    local -i max_attempts=5
-    local -i attempt_num=1
+REQUEST_URI="/dev/build"
 
-    until RESULT=$(lynx -dump http://silverstripe/dev/build)
+# Retries a command on failure.
+# $1 - the max number of attempts
+# $2... - the command to run
+
+retry() {
+    local -r -i max_attempts="$1"; shift
+    local -r cmd="$@"
+    local -i attempt_num=1
+    until $cmd
     do
         if ((attempt_num==max_attempts))
         then
-            echo "Attempt $attempt_num failed and there are no more attempts left!"
             return 1
         else
-            echo "Attempt $attempt_num failed! Trying again in $attempt_num seconds..."
             sleep $((attempt_num++))
         fi
     done
+}
+
+
+try_curl() {
+    RESULT=$(retry 5 lynx -dump http://silverstripe/dev/build)
 
     echo "${RESULT}"
+}
+
+try_cgi() {
+    RESULT=$(REQUEST_URI=${REQUEST_URI:-/dev/build} \
+    SCRIPT_FILENAME=${WORK_DIR:-/var/www/html}/index.php \
+    SERVER_PROTOCOL=HTTP/1.1 \
+    REQUEST_METHOD=GET \
+    QUERY_STRING=${QUERY_STRING:-} \
+    ./wait-for silverstripe:9000 --quiet --timeout=20 -- cgi-fcgi -bind -connect silverstripe:9000 | lynx --dump -stdin)
+
+    echo "${RESULT}"
+}
+
+fail() {
+    MESSAGE="Tests failed!"
+    echo "${MESSAGE}"
+    echo ""
+    echo "REQUEST_URI: ${REQUEST_URI:-}"
+    echo "WORK_DIR: ${WORK_DIR:-}"
+    echo "QUERY_STRING: ${QUERY_STRING:-}"
+    exit 1
+}
+
+pass() {
+    MESSAGE=${1:-Tests passed!}
+    echo "${MESSAGE}"
+    exit 0
 }
 
 cp .env /src/
@@ -25,25 +61,39 @@ cp _ss_environment.php /src/
 
 chown -R www-data:www-data /src
 
-./wait-for database:3306 --quiet --timeout=20
+echo " - Checking database connection..."
+retry 5 ./wait-for database:3306 --quiet --timeout=5
+echo " - Good"
+echo ""
 
-if [ "${DISTRO}" == "apache" ]; then
-    try_curl
-    if [[ "${RESULT}" =~ "Database build completed!" ]]; then
-        echo "Tests passed!"
-        exit 0
-    else
-        echo "Tests failed!"
-        exit 1
-    fi
-elif [ "${DISTRO}" == "fpm" ]; then
-    SCRIPT_NAME=/dev/build \
-    SCRIPT_FILENAME=/dev/build \
-    REQUEST_METHOD=GET \
-    ./wait-for silverstripe:9000 --quiet --timeout=20 -- cgi-fcgi -bind -connect silverstripe:9000
+case "${DISTRO}" in
+    apache)
+        try_curl
+        
+        if [[ "${RESULT}" =~ "Database build completed!" ]]; then
+            pass
+        fi
+        ;;
+    fpm)
+        WORK_DIR=${MOUNT_DIR:-/var/www/html}
+        [ -d "/src/public" ] && WORK_DIR="${WORK_DIR}/public"
+        
+        try_cgi
+        # Check if 302 and for devbuildtoken and retry
+        if [[ "${RESULT}" =~ "Status: 302" ]]; then
+            TOKEN=$(echo "${RESULT}" | sed -nr 's/.*devbuildtoken=([a-z0-9]+).*/\1/p')
+            QUERY_STRING="devbuildtoken=${TOKEN}"
+            try_cgi
+        fi
 
-    echo "Tests passed!"
-    exit 0
-fi
-echo "Tests skipped!"
-exit 0
+        if [[ "${RESULT}" =~ "Database build completed!" ]]; then
+            pass
+        fi
+        ;;
+    cli)
+        pass "Skipping CLI tests"
+        ;;
+esac
+
+# If we get here. Tests failed.
+fail
